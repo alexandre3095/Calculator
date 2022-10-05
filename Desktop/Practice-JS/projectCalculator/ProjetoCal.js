28dc040ce71c00c842ab9c542d6e22e5211dfbb1
Alexsander passos <92690936+alexandre3095@users.noreply.github.com>
2022-10-05 09:20:26 -0300

    Update ProjetoCal.js

--- Desktop/Practice-JS/projectCalculator/ProjetoCal.js
+++ Desktop/Practice-JS/projectCalculator/ProjetoCal.js
@@ -1,19 +1,17 @@
-/* Function insert for add a number in input of calculator*/
-
 function insert(num){
   let numero = document.getElementById('result').innerHTML;
   document.getElementById('result').innerHTML = numero + num; 
 }
-/*Function clean for the caractere "C" that will clean the input of calculator*/ 
+
 function clean(){
   document.getElementById('result').innerHTML = "";
 }
-/*Function back for the caractere "<" that will delete the numbers one by one*/ 
+
 function back(){
   let result = document.getElementById('result').innerHTML;
   document.getElementById('result').innerHTML= result.substring(0, result.length -1);
 }
-/*Function calc, this function will calculator the according to the values ​​entered */
+
 function calc (){
   let result = document.getElementById('result').innerHTML;
   if(result){
